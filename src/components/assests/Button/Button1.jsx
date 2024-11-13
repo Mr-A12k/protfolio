714c9e70ead71128100cea3212a0eb02875936fe
Mr-A12k <kabil448446@gmail.com>
2024-11-13 09:45:36 +0530

    mui

---
+++ src/components/assests/Button/Button1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import  Button from '@mui/material';
+
+const Button1 = () => {
+  return (
+    <div>
+       <Button variant="contained"></Button>
+       {/* <Button variant="outlined">Outlined</Button> */}
+    </div>
+  )
+}
+
+export default Button1;
--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import './Hero.css'; 
+import './Hero.css';
+import Kabix from '../assests/Kabix.png';
+//import { Button, Box, Typography, Container } from '@mui/material';
+import Button from '../assests/Button/Button1';
+
 
 const HeroSection = () => {
     return (
-        <section className="hero-section dark-mode">
-            <div className="hero-container">
-                <div className="hero-layout">
-                <div>
-                       <h2>Hello,</h2>
-                       <h1>I'm <span>Arun Kapil</span></h1>
-                        <p className="hero-text">
-                             a versatile UI/UX designer and full-stack web developer with a focus on crafting seamless, user-centric digital experiences
-                            <br className="hidden md:inline lg:hidden" />
-                            turpis pulvinar, est scelerisque ligula sem
+        <section className="hero-section">
+            <div className="container">
+                <div className="content-container">
+                    <div className="content">
+                        <h1 className="title">
+                            <div className="hello-text">
+                            Hello,
+                            </div>
+                            <br />
+                            <span className="highlight">I'm<span className='name-highlight'>Arun Kapil</span></span> Designer/Developer</h1>
+                        <p className="description">Passionate about creating seamless and impactful user experiences,
+                            I am a UI/UX designer and developer with a keen eye for detail and innovation.
+                            My journey spans diverse projects, from designing intuitive interfaces to building responsive web applications.
                         </p>
-                        <button>Contact Me</button>
                     </div>
-                    <img src="assets/svg/Business_SVG.svg" alt="Protfolio Image" />
+                    <div className="button-group">
+                        {/* <button variant="contained"><a href="#" className="btn-primary">Book a Call</a></button>
+                        <button><a href="#" className="btn-secondary">Download Resume</a></button> */}
+                        <Button></Button>
+                    </div>
+                </div>
+                <div className="image-container">
+                    <img src={Kabix} alt="Profile Image" className="hero-image" />
                 </div>
             </div>
         </section>
